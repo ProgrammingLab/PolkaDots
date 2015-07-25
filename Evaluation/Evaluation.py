8d49ace376112ba3647316e3edd76faee0b43442
otama-jaccy <funamin310@gmail.com>
2015-07-26 08:49:00 +0900

    Heredity

--- Evaluation/Evaluation.py
+++ Evaluation/Evaluation.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 __author__ = 'minto'
 
-from SizeEvaluation import SizeEvaluation
-from OverlapEvaluation import OverlapEvaluation
+from Evaluation.SizeEvaluation import SizeEvaluation
+from Evaluation.OverlapEvaluation import OverlapEvaluation
 
 
 class Evaluation:
@@ -13,6 +13,6 @@ class Evaluation:
 
     def evaluate(self, max_score, circles, picture_data):
         score = 0
-        score += SizeEvaluation().evaluate(10, circles)
+        score += SizeEvaluation().evaluate(10, circles, picture_data)
         score += OverlapEvaluation().evaluate(10, circles, picture_data)
         return score
