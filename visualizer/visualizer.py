8e3ca6a5f6732c179b8596fdb89edde5c8c7a197
Taisuke Miyazaki <imishinist@gmail.com>
2015-07-26 23:40:57 +0900

    ファイル選択可能に変更

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -3,8 +3,9 @@ __author__ = 'taisuke'
 import cv2
 import numpy as np
 from form import Ui_Form
+from PyQt5.QtCore import QRect
 from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtWidgets import QApplication, QWidget, QGraphicsScene
+from PyQt5.QtWidgets import QApplication, QWidget, QGraphicsScene, QFileDialog, QGraphicsPixmapItem
 
 
 class Visualizer(QWidget, Ui_Form):
@@ -15,12 +16,33 @@ class Visualizer(QWidget, Ui_Form):
         self.ui = Ui_Form()
         self.setupUi(self)
 
+        self.fileSelect.clicked.connect(self.fileSelected)
+
     # imageはopencvのオブジェクト
     def drawImage(self, image):
         height, width, dim = image.shape
         image = QImage(image, width, height, dim * width, QImage.Format_RGB888)
         self.scene = QGraphicsScene()
-        self.scene.addItem(QGraphicsPixMapItem(QPixmap.fromImage(image)))
+        self.scene.addItem(QGraphicsPixmapItem(QPixmap.fromImage(image)))
+        self.imageView.setScene(self.scene)
+
+    def fileSelected(self):
+        file = QFileDialog.getOpenFileName()
+        if file[0]:
+            self.file = file[0]
+            self.fileEdit.setText(self.file)
+            image_item = QGraphicsPixmapItem(QPixmap(self.file))
+            __width = image_item.boundingRect().width()
+            __height = image_item.boundingRect().height()
+            __x = self.imageView.x()
+            __y = self.imageView.y()
+
+            self.imageView.setGeometry(QRect(__x, __y, __width, __height))
+            __main_x = max(int(__x + __width + 10), 630)
+            __main_y = int(__y + __height + 10)
+            self.resize(__main_x, __main_y)
+            self.scene.addItem(image_item)
+            self.imageView.setScene(self.scene)
 
 
 if __name__ == '__main__':
